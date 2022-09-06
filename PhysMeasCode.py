926a4e5343fb5e238a09f4acb308d75845f47251
GTownsley <98366066+GTownsley@users.noreply.github.com>
2022-09-06 13:36:34 -0500

    Update PhysMeasCode.py

--- PhysMeasCode.py
+++ PhysMeasCode.py
@@ -1,6 +1,7 @@
 #import libraries
 import pandas as pd
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 import time
 
@@ -9,7 +10,7 @@ starttime = time.time()
 #creates path to the file downloaded directly from lab computer
 path = r"C:\Users\gtown\OneDrive\Documents\muon.txt"
 #converts file to .csv, so it's usuable in Python
-muondata = pd.read_csv(path, header=None, prefix = 'Col_', nrows=100, usecols=[0], sep=' ')
+muondata = pd.read_csv(path, header=None, prefix = 'Col_', usecols=[0], sep=' ')
 
 #organize data into something useful
 arr = muondata.values
@@ -19,20 +20,31 @@ while i<len(arr):
 	if arr[i]<40000:
 		decays.append(arr[i])
 		i=i+1
-		continue
 	else:
 		i=i+1
-		continue
 
-mean = np.average(decays)
+tobs = np.average(decays)
 stdev = np.std(decays)
-sterr = stdev/(np.sqrt(len(decays))
+utobs = stdev/(np.sqrt(len(decays)))
 
-#print(mean)
-#print(stdev)
-#print(sterr)
+tp = 2140
+tm = 2030
+utp = 170
+utm = 10
+
+rho = -(tp/tm)*((tm*tobs)/(tp-tobs))
+a = ((utp*tobs)/(tp-tobs))*(tp/(tp-tobs)-1)
+b = ((utobs*tp)/(tp-tobs))*(tobs/(tp-tobs)+1)
+c = a**2 + b**2
+urho = math.sqrt(c)
+
+print(tobs)
+print(stdev)
+print(utobs)
+print(rho)
+print(urho)
 
 #plt.hist(decays, bins = 10)
 #plt.show()
 
-#print(time.time()-starttime)
+print(time.time()-starttime)
